f2fb84a91354471f14ec4eb0dcaa567d90e65bf7
JoachimPuercher <joachim.puercher@gmail.com>
2026-05-28 10:42:40 +0200

    feat(boards): add CRUD (patch, delete) endpoints

--- boards_app/api/urls.py
+++ boards_app/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BoardListView
+from .views import BoardListView, BoardDetailView
 
 urlpatterns = [
     path('boards/', BoardListView.as_view()),
+    path('boards/<int:pk>/', BoardDetailView.as_view())
 ]
