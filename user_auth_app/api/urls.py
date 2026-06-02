13b3e7b1e2a7a4c9d7a21637ec5506f4579da8d2
JoachimPuercher <joachim.puercher@gmail.com>
2026-06-02 13:39:24 +0200

    feat(emailcheck): add check email per query param endpoint

--- user_auth_app/api/urls.py
+++ user_auth_app/api/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import registration_view, login_view
+from .views import registration_view, login_view, email_check_view
 
 urlpatterns = [
     path('registration/', registration_view),
-    path('login/', login_view)
+    path('login/', login_view),
+    path('email-check/', email_check_view)
 ]
