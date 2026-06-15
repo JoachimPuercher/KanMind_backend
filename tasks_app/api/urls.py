efffe2652f72fbc0eb89a9655ad333e6e34b19a5
JoachimPuercher <joachim.puercher@gmail.com>
2026-06-15 10:51:15 +0200

    Add trailing slash to comment delete route

--- tasks_app/api/urls.py
+++ tasks_app/api/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('tasks/assigned-to-me/', TaskListSelfAssignedView.as_view()),
     path('tasks/', PostTaskView.as_view()),
     path('tasks/<int:task_id>/comments/', TaskCommentView.as_view()),
-    path('tasks/<int:task_id>/comments/<int:comment_id>', DeleteCommentView.as_view())
+    path('tasks/<int:task_id>/comments/<int:comment_id>/', DeleteCommentView.as_view())
 ]
