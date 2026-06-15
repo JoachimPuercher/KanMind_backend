8edbf61b0558c4f615bd3cc00f139451e511226e
JoachimPuercher <joachim.puercher@gmail.com>
2026-06-15 09:21:35 +0200

    Add comment list/create and delete views with serializer and route

--- tasks_app/api/urls.py
+++ tasks_app/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import PostTaskView, TaskListSelfAssignedView
+from .views import TaskCommentView
 
 urlpatterns = [
     path('tasks/assigned-to-me/', TaskListSelfAssignedView.as_view()),
     path('tasks/', PostTaskView.as_view()),
+    path('tasks/<int:task_id>/comments/', TaskCommentView.as_view())
 ]
--- tasks_app/api/views.py
+++ tasks_app/api/views.py
@@ -1,9 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, generics
-from .serializers import TaskSerializer
-from ..models import Board
+from .serializers import TaskSerializer, CommentSerializer
+from ..models import Board, Comment
+from tasks_app.models import Task
+from django.contrib.auth.models import User
 from .permission import IsBoardMember
+from django.shortcuts import get_object_or_404
+
 # from .serializers import GetBoardSerializer, PostBoardSerializer
 from django.db.models import Q
 # from django.shortcuts import get_object_or_404
@@ -20,9 +24,27 @@ class TaskListSelfAssignedView(APIView):
     
 
 class PostTaskView(generics.CreateAPIView):
+    # permission_classes = [IsBoardMember]
     serializer_class = TaskSerializer
-    permission_classes = [IsBoardMember]
+
+
 
 class UpdateDeleteTaskView(generics.DestroyAPIView, generics.UpdateAPIView):
     serializer_class = TaskSerializer
     permission_classes = []
+
+
+class TaskCommentView(generics.ListCreateAPIView):
+    permission_classes = []
+    serializer_class = CommentSerializer
+    
+    def get_queryset(self):
+        return Comment.objects.filter(task_id=self.kwargs["task_id"]).all()
+
+    def perform_create(self, serializer):
+        task = get_object_or_404(Task, pk=self.kwargs["task_id"])
+        serializer.save(author=self.request.user, task=task)
+
+class DeleteCommentView(generics.DestroyAPIView):
+    permission_classes = [IsBoardMember]
+    
