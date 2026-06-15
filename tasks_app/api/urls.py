02b1b5bc628a651ebae22b159840b3bfa2f02cc1
JoachimPuercher <joachim.puercher@gmail.com>
2026-06-15 10:50:33 +0200

    Implement comment delete view with owner permission and route

--- tasks_app/api/urls.py
+++ tasks_app/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostTaskView, TaskListSelfAssignedView
+from .views import PostTaskView, TaskListSelfAssignedView, DeleteCommentView
 from .views import TaskCommentView
 
 urlpatterns = [
     path('tasks/assigned-to-me/', TaskListSelfAssignedView.as_view()),
     path('tasks/', PostTaskView.as_view()),
-    path('tasks/<int:task_id>/comments/', TaskCommentView.as_view())
+    path('tasks/<int:task_id>/comments/', TaskCommentView.as_view()),
+    path('tasks/<int:task_id>/comments/<int:comment_id>', DeleteCommentView.as_view())
 ]
