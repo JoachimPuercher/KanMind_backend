a9ec9e40ba89ff344b75809c40c3062f580922cd
JoachimPuercher <joachim.puercher@gmail.com>
2026-06-25 14:48:58 +0200

    feat(tasks): add get reviewer task list endpoint

--- tasks_app/api/urls.py
+++ tasks_app/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import PostTaskView, TaskListSelfAssignedView, DeleteCommentView, TaskCommentView, UpdateDeleteTaskView
+from .views import PostTaskView, TaskListSelfAssignedView, DeleteCommentView, TaskCommentView, UpdateDeleteTaskView, TasksReviewingList
 
 
 urlpatterns = [
     path('tasks/assigned-to-me/', TaskListSelfAssignedView.as_view()),
     path('tasks/', PostTaskView.as_view()),
+    path('tasks/reviewing/', TasksReviewingList.as_view()),
     path('tasks/<int:task_id>/', UpdateDeleteTaskView.as_view()),
     path('tasks/<int:task_id>/comments/', TaskCommentView.as_view()),
     path('tasks/<int:task_id>/comments/<int:comment_id>/', DeleteCommentView.as_view())
